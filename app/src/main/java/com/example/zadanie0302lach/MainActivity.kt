940397b9702f60606aaf1f285dd5e46a4ab67c4d
kamcio420 <kamcio420@gmail.com>
2022-02-03 14:33:26 +0100

    2

--- app/src/main/java/com/example/zadanie0302lach/MainActivity.kt
+++ app/src/main/java/com/example/zadanie0302lach/MainActivity.kt
@@ -2,33 +2,31 @@ package com.example.zadanie0302lach
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.zadanie0302lach.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+    class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+        }
 
-    private lateinit var binding: ActivityMainBinding
+        private fun setupAutoCompleteTextView() {
+            val button = findViewById<Button>(R.id.btnSubmit)
+            val textInput = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+            val countries = resources.getStringArray(R.array.countries_array)
+            // Create the adapter and set it to the AutoCompleteTextView
+            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, countries)
+            button.setOnClickListener { textInput.setAdapter(adapter) }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+            button.setOnClickListener {
+                val enteredText = getString(R.string.entered_text) + " " + textInput.text
+                Toast.makeText(this@MainActivity, enteredText, Toast.LENGTH_SHORT).show()
 
-        setupAutoCompleteTextView()
-    }
-
-    private fun setupAutoCompleteTextView() {
-        val countries = resources.getStringArray(R.array.countries_array)
-        // Create the adapter and set it to the AutoCompleteTextView
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, countries)
-        binding.autoCompleteTextView.setAdapter(adapter)
-
-        binding.btnSubmit.setOnClickListener {
-            val text = getString(R.string.entered_text) + " " + binding.autoCompleteTextView.text
-            Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
+            }
         }
     }
-}
+
 
